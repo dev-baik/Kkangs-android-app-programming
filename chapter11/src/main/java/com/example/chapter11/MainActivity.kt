4b0c80a742067411407269fecc6c7c5803afc2f8
wlsdn9297 <wlsdn9297@naver.com>
2022-07-30 11:21:30 +0900

    시계 앱의 스톱워치 기능 만들기

---
+++ chapter11/src/main/java/com/example/chapter11/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.chapter11
+
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.chapter11.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        // ActionBarDrawerToggle 버튼 적용
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+
+        // 뷰페이저에 어댑터 적용
+        val adapter = MyFragmentPagerAdatper(this)
+        binding.viewpager.adapter = adapter
+    }
+
+    class MyFragmentPagerAdatper(activity: FragmentActivity): FragmentStateAdapter(activity) {
+        private val fragments: List<Fragment> = listOf(OneFragment(), TwoFragment(), ThreeFragment())
+
+        override fun getItemCount(): Int {
+            return fragments.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragments[position]
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        // MenuItem 객체를 더고 그 안에 포함된 ActionView 객체 획득
+        val menuItem = menu?.findItem(R.id.menu_search)
+        val searchView = menuItem?.actionView as SearchView
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // 검색어 변경 이벤트
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // 키보드의 검색 버튼을 클릭한 순간의 이벤트
+                Log.d("kkang", "search text : $newText")
+                return true
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // 이벤트가 토글 버튼에서 발생하면
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
